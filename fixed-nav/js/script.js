0a0a79e983976f25d35fbb6159c9763167e86e5d
Olivier Valette <olivier.valette@spi10.com>
2019-02-06 15:14:42 +0100

    fixed header

--- fixed-nav/js/script.js
+++ fixed-nav/js/script.js
@@ -1,6 +1,22 @@
-// assure que la page est chargée pour exécuter les scripts
-// surtout pour ceux qui interagissent avec le DOM
-// version abrégée : $(function() {});
-$(document).ready(function () {
-    console.log('ICI');
+$(document).ready (function () {
+
+    $('html body').append('<button class="btt-btn">Haut de page</button>');
+    $('.btt-btn').hide();
+
+    $('.btt-btn').click(function () {
+        $('html, body').animate({
+            scrollTop: 0
+        }, 500);
+    });
+
+    $(window).scroll(function () {
+        if ( $(this).scrollTop() > 300 ) {
+            $('.btt-btn').fadeIn();
+            $('body').addClass('scrolled');
+        } else {
+            $('.btt-btn').fadeOut();
+            $('body').removeClass('scrolled');
+        }
+    })
+
 });
